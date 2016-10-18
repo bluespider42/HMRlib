a89976575901bca7eccadf4f979f025e77510057
Ben <bluespider42@hotmail.co.uk>
2016-10-18 14:43:15 +0100

    Updated Readme and example

--- README.md
+++ README.md
@@ -1,6 +1,36 @@
+[![Build Status](https://travis-ci.org/bluespider42/HMRlib.svg?branch=develop)](https://travis-ci.org/bluespider42/HMRlib)
+[![Build Status](https://gitlab.com/obic/HMRlib/badges/develop/build.svg)](https://gitlab.com/obic/HMRlib/commits/develop)
 HMRlib
 ======
-[![Build Status](https://travis-ci.org/bluespider42/HMRlib.svg?branch=develop)](https://travis-ci.org/bluespider42/HMRlib)
-[![build status](https://gitlab.com/obic/HMRlib/badges/develop/build.svg)](https://gitlab.com/obic/HMRlib/commits/develop)
+An Arduino library for use with Honeywell HMR Compass Modules.
+Currently only the HMR3300 is supported.
 
-Currently work in progress
+Currently work in progress.
+Work will be slow as my project now uses the HMC6343 sensor instead.
+
+Usage
+-----
+Open a serial port at a baudrate of 19200 to communicate with the sensor.
+Feed `encode(char)` one byte at a time.
+It will return true once an entire sentence has been parsed.
+
+Run
+ `get_data(long &pitch, long &roll, long &heading, unsigned long &age)`
+to update the variables with the new data.
+
+```
+#include <HMRlib.h>
+HMR3300 hmr
+void setup() {
+    long pitch, roll, heading;
+    unsigned long age;
+    Serial.begin(19200);
+}
+void loop() {
+    while(Serial.available()) {
+        c = Serial.read();
+        if (hmr.encode(c))
+            hmr.get_data(&pitch, &roll, &heading, &age);
+    }
+}
+```
--- examples/printData/printData.ino
+++ examples/printData/printData.ino
@@ -1,6 +1,5 @@
 #include <HMRlib.h>
 
-
 #if defined(__AVR_ATmega328P__)
     #include "SoftwareSerial.h"
     SoftwareSerial Serial1(0, 1);
@@ -8,25 +7,43 @@
 
 HMR3300 hmr;
 
-long tiltx, tilty, head;
+long pitch, roll, head;
 unsigned long data_age;
 
 void setup() {
     Serial.begin(115200);
+    while (!Serial){
+        ; //wait for serial port
+    }
     Serial1.begin(19200);
+    delay(1000);
+    Serial.println("Starting...");
 }
 
 void loop() {
-    while (Serial1.available()) {
-        int c = Serial1.read();
-        if (hmr.encode(c)) {
-            hmr.get_data(&tiltx, &tilty, &head, &data_age);
-            Serial.print("X Angle: ");
-            Serial.print(tiltx);
-            Serial.print(", Y Angle: ");
-            Serial.print(tilty);
-            Serial.print(", Bearing: ");
-            Serial.println(head);
+    bool newData = false;
+    for (unsigned long start = millis(); millis() - start <1000;) {
+        while (Serial1.available()) {
+            char c = Serial1.read();
+            //Serial.print(c);
+            if (hmr.encode(c)) {
+                newData = true;
+            }
         }
+    }
+    if (newData) {
+        hmr.get_data(&pitch, &roll, &head, &data_age);
+        Serial.print("Pitch: ");
+        Serial.print(float(pitch)/100);
+        Serial.write(248);
+        Serial.print(", Roll: ");
+        Serial.print(float(roll)/100);
+        Serial.write(248);
+        Serial.print(", Bearing: ");
+        Serial.print(float(head)/100);
+        Serial.write(248);
+        Serial.print(", Age: ");
+        Serial.print(float(data_age)/1000);
+        Serial.println("s");
     }
 }
